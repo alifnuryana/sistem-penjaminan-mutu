746de4652924ef299070e2815c43b1eb0001f1ef
Alif Nuryana <alifnuryana0@gmail.com>
2023-05-13 12:21:00 +0700

    :wrench: konfigurasi aliases dalam vite

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+    resolve: {
+        alias: {
+            '@': '/resources/js',
+        }
+    },
     plugins: [
         laravel({
             input: 'resources/js/app.js',
