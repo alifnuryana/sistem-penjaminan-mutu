0a95167b42550aa51aa5a25336bbf2319112865f
husnirobani <husnir2005@gmail.com>
2023-05-18 09:15:59 +0700

    add GetAllDecree static method to get decree data

--- app/Http/Controllers/DecreeController.php
+++ app/Http/Controllers/DecreeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Actions\GetAllDecree;
 use App\Models\Decree;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
@@ -11,22 +12,9 @@ class DecreeController extends Controller
 {
     public function index(){
 
-        $decrees = Decree::all()->map(function ($decree){
-            return[
-                'name' => $decree->name,
-                'code' => $decree->code,
-                'type' => $decree->type,
-                'url' => Storage::url($decree->file_path),
-                'path' => $decree->file_path,
-                'validity_date' => $decree->validity_date,
-                'current_date' => (new \DateTime())->format('Y-m-d')
-            ];
-        })->sortByDesc('validity_date')->values()->all();
-//        dd($decrees);
-//        $file = Decree::first()->file_path;
-//        return \Storage::url($file);
+
         return Inertia::render('Data/Decree/index', [
-            'decrees' => $decrees
+            'decrees' => GetAllDecree::run()
         ]);
     }
 
