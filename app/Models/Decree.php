8737a3aa61cc0886afe91a0e220df9195666e7d8
husnirobani <husnir2005@gmail.com>
2023-05-18 21:46:26 +0700

    add validity_date & release_date to fillable

--- app/Models/Decree.php
+++ app/Models/Decree.php
@@ -12,7 +12,7 @@ class Decree extends Model
     use HasUuids;
 
     protected $fillable = [
-        'code', 'name', 'decreeable_type', 'decreeable_id', 'file_path', 'type', 'size'
+        'code', 'name', 'decreeable_type', 'decreeable_id', 'file_path', 'type', 'size', 'release_date', 'validity_date'
     ];
 
     public function decreeable() : MorphTo
