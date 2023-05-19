37f73d9947917de9ab01f99a4064084710ecbf02
Alif Nuryana <alifnuryana0@gmail.com>
2023-05-19 14:31:40 +0700

    :construction: WIP conflict

--- resources/js/Components/Layouts/Partials/Header.vue
+++ resources/js/Components/Layouts/Partials/Header.vue
@@ -16,8 +16,8 @@
                                 class="hs-dropdown-toggle inline-flex flex-shrink-0 justify-center items-center gap-2 h-[2.375rem] w-[2.375rem] rounded-full font-medium bg-white text-gray-700 align-middle hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2 focus:ring-offset-white transition-all text-xs dark:bg-gray-800 dark:hover:bg-slate-800 dark:text-gray-400 dark:hover:text-white dark:focus:ring-gray-700 dark:focus:ring-offset-gray-800">
                             <img
                                 class="inline-block h-[2.375rem] w-[2.375rem] rounded-full ring-2 ring-white dark:ring-gray-800"
-                                src="https://images.unsplash.com/photo-1568602471122-7832951cc4c5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=320&h=320&q=80"
-                                alt="Image Description">
+                                :src="`https://ui-avatars.com/api/?name=${page.props.user.name}&color=7F9CF5&background=EBF4FF`"
+                                :alt="page.props.user.name">
                         </button>
 
                         <div
@@ -25,8 +25,8 @@
                             aria-labelledby="hs-dropdown-with-header">
                             <div class="py-3 px-5 -m-2 bg-gray-100 rounded-t-lg dark:bg-gray-700">
                                 <p class="text-sm text-gray-500 dark:text-gray-400">Masuk sebagai</p>
-                                <!-- TODO: implementasikan shared data dan mengambil instance user pada shared data -->
-                                <p class="text-sm font-medium text-gray-800 dark:text-gray-300">james@site.com</p>
+                                <p class="text-sm font-medium text-gray-800 dark:text-gray-300">
+                                    {{ page.props.user.email }}</p>
                             </div>
                             <div class="mt-2 py-2 first:pt-0 last:pb-0">
                                 <Link :href="route('logout')"
@@ -46,6 +46,8 @@
 </template>
 
 <script setup>
-import {Link} from "@inertiajs/vue3";
+import {Link, usePage} from "@inertiajs/vue3";
 import {ArrowLeftOnRectangleIcon} from "@heroicons/vue/24/outline/index.js";
+
+const page = usePage();
 </script>
