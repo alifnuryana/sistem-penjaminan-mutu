76f6ab63dc0dd2ea40e626d3b68dcbbd4c51e0a0
Alif Nuryana <alifnuryana0@gmail.com>
2023-05-19 09:19:19 +0700

    :fire: remove dead code

--- app/Models/StudyProgram.php
+++ app/Models/StudyProgram.php
@@ -3,14 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\MorphOne;
 
 class StudyProgram extends Model
 {
-    use HasFactory, HasUuids;
+    use HasUuids;
 
     protected $fillable = [
         'degree',
