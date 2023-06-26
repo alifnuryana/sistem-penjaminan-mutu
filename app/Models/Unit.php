3eecf1a2b2231b93231377022aa7f902c392ffa9
Alif Nuryana <alifnuryana0@gmail.com>
2023-06-26 15:26:24 +0700

    :recycle: create unit model to Notifiable

--- app/Models/Unit.php
+++ app/Models/Unit.php
@@ -6,10 +6,11 @@ use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
+use Illuminate\Notifications\Notifiable;
 
 class Unit extends Model
 {
-    use HasUuids;
+    use HasUuids, Notifiable;
 
     protected $fillable = [
         'code',
